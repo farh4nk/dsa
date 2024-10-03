aef46988738e638388569bbec447db8f6bd5dd8e
Farhan Khan <fk27179@gmail.com>
2024-10-03 14:57:05 -0400

    Create Sorts.java

---
+++ algorithms/Sorts.java
@@ -0,0 +1,16 @@
+public class Sorts {
+  public static void main(String[] args) {
+  }
+
+  public static void bubbleSort() {
+    /* WIP */
+  }
+
+  public static void insertionSort() {
+    /* WIP */
+  }
+
+  public static void selectionSort() {
+    /* WIP */
+  }
+}
