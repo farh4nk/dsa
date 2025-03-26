a4ee0f57c37a25d650d66b37e8cfbb09670d051e
Farhan Khan <fk27179@gmail.com>
2025-03-25 20:41:46 -0400

    Create 912.py

---
+++ leetcode/912.py
@@ -0,0 +1,45 @@
+# 912. Sort an Array
+# Given an array of integers nums, sort the array in ascending order and return it.
+# You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+# Example 1:
+# Input: nums = [5,2,3,1]
+# Output: [1,2,3,5]
+# Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), while the positions of other numbers are changed (for example, 1 and 5).
+# Example 2:
+# Input: nums = [5,1,1,2,0,0]
+# Output: [0,0,1,1,2,5]
+# Explanation: Note that the values of nums are not necessairly unique.
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        
+        def merge(arr, L, M, R):
+            left = arr[L:M+1]
+            right = arr[M+1:R+1]
+            i, j, k = L, 0, 0
+            while j < len(left) and k < len(right):
+                if left[j] <= right[k]:
+                    arr[i] = left[j]
+                    j += 1
+                else:
+                    arr[i] = right[k]
+                    k += 1
+                i += 1
+            while j < len(left):
+                arr[i] = left[j]
+                j += 1
+                i += 1
+            while k < len(right):
+                arr[i] = right[k]
+                k += 1
+                i += 1
+
+        def mergeSort(nums, l, r):
+            if l == r:
+                return nums
+            m = (l + r) // 2
+            mergeSort(nums, l, m)
+            mergeSort(nums, m+1, r)
+            merge(nums, l, m, r)
+            return nums
+        return mergeSort(nums, 0, len(nums))
