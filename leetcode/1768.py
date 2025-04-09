e4a7ed18cd979a2c44e6668c76fabfc135217043
Farhan Khan <fk27179@gmail.com>
2025-04-08 21:10:17 -0400

    Update 1768.py

--- leetcode/1768.py
+++ leetcode/1768.py
@@ -17,5 +17,24 @@ class Solution:
         result += word1[p1:]
         result += word2[p2:]
         return result
-            
+
+# ALTERNATIVE SOLUTION
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        res = ''
+        len1, len2 = len(word1), len(word2)
+        p1, p2 = 0, 0
+        while p1 < len1 and p2 < len2:
+            res+= word1[p1]
+            p1 +=1
+            res += word2[p2]
+            p2 += 1
+        if len1 > len2:
+            res += word1[p1:]
+        elif len1 < len2:
+            res += word2[p2:]
+        return res
+
+
 
