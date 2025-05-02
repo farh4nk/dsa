b4c25f719c07a5f125d46f3c9b3ed88a29923a08
Farhan Khan <fk27179@gmail.com>
2025-05-01 23:24:55 -0400

    Create 704.py

---
+++ leetcode/704.py
@@ -0,0 +1,26 @@
+# 704. Binary Search
+# You are given an array of distinct integers nums, sorted in ascending order, and an integer target.
+# Implement a function to search for target within nums. If it exists, then return its index, otherwise, return -1.
+# Your solution must run in O(logn) time.
+# Example 1:
+# Input: nums = [-1,0,2,4,6,8], target = 4
+# Output: 3
+# Example 2:
+# Input: nums = [-1,0,2,4,6,8], target = 3
+# Output: -1
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        return self.binary(nums, 0, len(nums) -1, target )
+        
+        
+    def binary(self, nums, left, right, target):
+        mid = left + (right-left) // 2
+        if target not in nums:
+            return -1
+        if (nums[mid] == target):
+            return mid
+        elif (target < nums[mid]):
+            return self.binary(nums, 0, mid, target)
+        elif (target > nums[mid]):
+            return self.binary(nums, mid+1, right, target)
