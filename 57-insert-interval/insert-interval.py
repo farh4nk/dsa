4eb86782ceb8a3dff1deefb6cd85d8c77fa8b32e
Farhan Khan <fk27179@gmail.com>
2026-09-04 01:06:45 -0400

    Time: 3 ms (29.82%) | Memory: 21.3 MB (57.38%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,23 +1,38 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        res = []
-        # iterate through intervals, 3 cases:
-        # 1. newInterval comes before current interval
-        # 2. newInterval comes after current interval
-        # 3. newInterval overlaps with current interval
 
-        for i, interval in enumerate(intervals):
-            # case 1
-            if newInterval[1] < interval[0]:
-                res.append(newInterval)
-                return res + intervals[i:]
-            # case 2
-            elif newInterval[0] > interval[1]:
-                res.append(interval)
-            # case 3
-            else:
-                merged = [min(newInterval[0], interval[0]), max(newInterval[1], interval[1])]
-                newInterval = merged
-        res.append(newInterval)
+        if not intervals:
+            return [newInterval]
+        
+        def merge(intervals):
+            res = []
 
-        return res
+            sorted_intervals = sorted(intervals, key=lambda x: x[0])
+
+            res.append(sorted_intervals[0])
+
+            for i in range(1, len(intervals)):
+                curr = sorted_intervals[i]
+                prev = res[-1]
+
+                if curr[0] <= prev[1]:
+                    res[-1][1] = max(curr[1], res[-1][1])
+                else:
+                    res.append(curr)
+
+            return res
+
+        new = []
+        for i in range(len(intervals)):
+            inserted = False
+
+            if not inserted and intervals[i][0] > newInterval[0]:
+                new.append(newInterval)
+                inserted = True
+
+            new.append(intervals[i])
+
+        if len(new) == len(intervals):
+            new.append(newInterval)
+
+        return merge(new)
